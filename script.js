5d1d9b3cc4d5909571d1301a700013d382c651a3
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-22 22:48:50 -0700

    date is in the console!

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const date = document.getElementById("date");
 const day = document.getElementById("day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
+
+const today = new Date();
+
+console.log(today);
