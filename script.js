4a36acf82bb31043604bb208d158b6c593848972
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-22 22:53:18 -0700

    methods deployed but need to fix the display, which is all numbers!

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ const year = document.getElementById("year");
 const today = new Date();
 
 console.log(today);
+
+date.innerHTML = today.getDate();
+day.innerHTML = today.getDay();
+month.innerHTML = today.getMonth();
+year.innerHTML = today.getYear();
